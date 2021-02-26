9e11c742e1057f340edebbaccea221240580076e
jczujko <jarek.czujko@hotmail.com>
2021-02-26 05:41:38 +0100

    Poprawiono walidacje, dodano czyszczenie formularza

--- add.js
+++ add.js
@@ -24,31 +24,51 @@ function validateAddForm() {
     let formYear = document.getElementById("formYear");
     let formGenre = document.getElementById("formGenre");
     let formSummary = document.getElementById("formSummary");
+    let eval1 = false,
+        eval2 = false,
+        eval3 = false,
+        eval4 = false;
     if (formTitle.value === "") {
         alert("Field \"Title\" is required");
+    } else {
+        eval1 = true;
     }
     if (formYear.value === "") {
         alert("Field \"Year\" is required");
     } else if (!/^[0-9]{4}$/.test(formYear.value)) {
         alert("Year must be a correct 4 digit number");
+    } else {
+        eval2 = true;
     }
     if (formGenre.value === "") {
         alert("Field \"Genre\" is required");
+    } else {
+        eval3 = true;
     }
-    let randId = 0;
-    do {
-        randId = anotherMoviesList.movies.length * Math.floor(Math.random() * 10);
-        console.log(randId);
-    } while (anotherMoviesList.movies.findIndex(element => element.id === randId) != -1)
-    let movieInstance = {};
-    movieInstance.id = randId;
-    movieInstance.title = formTitle.value;
-    movieInstance.year = formYear.value;
-    movieInstance.genre = formGenre.value;
-    movieInstance.summary = formSummary.value;
-    movieInstance.seen = formSeen.value;
-    anotherMoviesList.set(movieInstance);
-    countMovies();
+    if (anotherMoviesList.movies.findIndex(element => element.title === formTitle.value) != -1) {
+        alert("Movie with same title is already on the list");
+    } else {
+        eval4 = true;
+    }
+    if (eval1 && eval2 && eval3 && eval4) {
+        let randId = 0;
+        do {
+            randId = anotherMoviesList.movies.length * Math.floor(Math.random() * 10);
+        } while (anotherMoviesList.movies.findIndex(element => element.id === randId) != -1)
+        let movieInstance = {};
+        movieInstance.id = randId;
+        movieInstance.title = formTitle.value;
+        movieInstance.year = formYear.value;
+        movieInstance.genre = formGenre.value;
+        movieInstance.summary = formSummary.value;
+        movieInstance.seen = formSeen.value;
+        anotherMoviesList.set(movieInstance);
+        formTitle.value = "";
+        formGenre.value = "";
+        formYear.value = "";
+        formSummary.value = "";
+        countMovies();
+    }
 }
 
 window.validateAddForm = validateAddForm;
